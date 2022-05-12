b3eebd25c96e3a9ea9b386c5f99125ae5f86889f
connelldan <danielcconnell@gmail.com>
2022-05-11 20:49:08 -0600

    new test

--- cloud_run_apps.tf
+++ cloud_run_apps.tf
@@ -26,9 +26,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.danielconnl_website.location
-  project     = google_cloud_run_service.danielconnl_website.project
-  service     = google_cloud_run_service.danielconnl_website.name
+  location = google_cloud_run_service.danielconnl_website.location
+  project  = google_cloud_run_service.danielconnl_website.project
+  service  = google_cloud_run_service.danielconnl_website.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ module "connelld_app_services" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 10.1"
 
-  name                = "app-services"
-  project_id          = "connelld-app-services"
-  billing_account     = var.billing_account
-  org_id              = var.org_id
+  name            = "app-services"
+  project_id      = "connelld-app-services"
+  billing_account = var.billing_account
+  org_id          = var.org_id
 
   activate_apis = [
     "run.googleapis.com",
